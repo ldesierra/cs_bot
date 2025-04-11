fd420ca54f10f0b27911ca2198914bd326a660e6
Lucas de sierra <83076120+ldesierra@users.noreply.github.com>
2025-04-10 23:59:33 -0300

    sssssssssssssdfssddsdsddsdssdssdssssddssddssssddssddssdssdsdssddssdssddssdsddssdsdssdsdsdssdsdss

--- app/services/api_checker.rb
+++ app/services/api_checker.rb
@@ -34,13 +34,17 @@ class ApiChecker
 
   def self.call
     fetch_and_wait
+    sleep 20
     fetch_and_wait
+    sleep 20
     fetch_and_wait
+    sleep 20
     fetch_and_wait
+    sleep 20
     fetch_and_wait
+    sleep 20
     fetch_and_wait
-    fetch_and_wait
-    fetch_and_wait
+    sleep 20
     fetch_and_wait
   end
 
@@ -49,12 +53,9 @@ class ApiChecker
     response = call_empire_api
 
     if response.code == 200
-
       katowice_2015_items = get_katowice_2015_items(response)
       katowice_2014_items = get_katowice_2014_items(response)
-      high_floats = get_high_float_items(response)
       low_floats = get_low_float_items(response)
-      special_items = get_special_items(response)
 
       if katowice_2015_items.any?
         messages << "Katowice 2015 items found: #{katowice_2015_items.map { |item| "#{item["market_name"]} with stickers #{ item["stickers"]&.pluck("name") }" }.join(', ')}"
@@ -62,16 +63,11 @@ class ApiChecker
         messages << "Low floats found: #{low_floats.map { |item| item["market_name"] }.join(', ')}"
       elsif katowice_2014_items.any?
         messages << "Katowice 2014 items found: #{katowice_2014_items.map { |item| "#{item["market_name"]} with stickers #{ item["stickers"]&.pluck("name") }" }.join(', ')}"
-      elsif high_floats.any?
-        messages << "High floats found: #{high_floats.map { |item| item["market_name"] }.join(', ')}"
-      elsif special_items.any?
-        puts "Special items found: #{special_items.map { |item| item["market_name"] }.join(', ')}"
       end
 
       messages
     else
-
-      send_telegram_message(response.code)
+      puts "Error: #{response.code}"
     end
   end
 
@@ -83,23 +79,8 @@ class ApiChecker
     response["data"].filter { |item| item["stickers"]&.pluck("name")&.any? {|s| s&.include?("Katowice 2014") } }
   end
 
-  def self.get_gold_items(response)
-    response["data"].filter do |item|
-      count = item["stickers"]&.pluck("name")&.count { |s| s&.include?("Gold") }
-      count.present? && count > 2 && !item["market_name"].include?("Souvenir")
-    end
-  end
-
-  def self.get_special_items(response)
-    response["data"].filter { |item| item["market_name"].include?("s") }
-  end
-
   def self.get_low_float_items(response)
     response["data"].filter { |item| item["wear"] && item["wear"] <= 0.001 }
-  end
-
-  def self.get_high_float_items(response)
-    response["data"].filter { |item| item["wear"].present? && item["wear"] == 0.999 }
   end
 
   def self.call_empire_api

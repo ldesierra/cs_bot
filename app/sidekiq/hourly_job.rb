da3e0c58eeb89895f6dbbffcf37ea3d3cf0a0b29
Lucas de sierra <83076120+ldesierra@users.noreply.github.com>
2025-04-22 10:38:14 -0300

    sdfsddsddsfdsdssdsdsdsssdfsdsdsdss

--- app/sidekiq/hourly_job.rb
+++ app/sidekiq/hourly_job.rb
@@ -13,28 +13,39 @@ class HourlyJob
 
     page = 1
     not_finished = true
+    messages = []
 
-    while not_finished
-      messages = []
-
+    while not_finished &&
       response = call_empire_api(page)
       if response.code == 200
         if response["data"].empty?
           not_finished = false
         else
           low_floats = get_low_float_items(response)
+          katowice_2014_items = get_katowice_2014_items(response)
 
           if low_floats.any?
-            messages << "found: #{low_floats.map { |item| "#{item["market_name"]} - #{item["id"]}" }.join(', ')}"
+            messages << "found: #{low_floats.map { |item| "#{item["market_name"]} - #{item["id"]} with price #{ item["purchase_price"].to_f / 160 }" }.join(', ')}"
+          end
+          if katowice_2014_items.any?
+            messages << "found: #{katowice_2014_items.map { |item| "#{item["market_name"]} - #{item["id"]} with stickers #{ item["stickers"]&.pluck("name") } with price #{ item["purchase_price"].to_f / 160 }" }.join(', ')}"
           end
         end
       end
 
       page = page + 1
     end
+
+    messages.each do |message|
+      send_telegram_message(message)
+    end
   end
 
   private
+
+  def get_katowice_2014_items(response)
+    response["data"].filter { |item| item["stickers"]&.pluck("name")&.any? {|s| s&.include?("Katowice 2014") } }
+  end
 
   def get_low_float_items(response)
     response["data"].filter { |item| item["wear"] && item["wear"] <= 0.001 }

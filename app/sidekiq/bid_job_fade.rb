56c69e0c5ce1289f107c327bfa4aae3135919253
Lucas de sierra <83076120+ldesierra@users.noreply.github.com>
2025-08-04 12:05:08 -0300

    sdfsfdssdssdsdss

--- app/sidekiq/bid_job_fade.rb
+++ app/sidekiq/bid_job_fade.rb
@@ -6,21 +6,39 @@ class BidJobFade
 
   API_URL = 'https://your-endpoint-x.com'
   TELEGRAM_TOKEN = ENV["TELEGRAM_BOT_TOKEN"]
+  TELEGRAM_TOKEN_2 = ENV["TELEGRAM_BOT_TOKEN_2"]
   TELEGRAM_CHAT_ID = ENV["TELEGRAM_CHAT_ID"]
+  TELEGRAM_CHAT_ID_BRO = ENV["TELEGRAM_CHAT_ID_BRO"]
 
   def perform(item, special = false)
-    message = item_message(item)
-    send_telegram_message(message) if message.present?
+    begin
+      message = item_message(item)
+      send_telegram_message(message) if message.present?
 
-    should_bid = if special
-                   (item["fade_percentage"].to_f >= 98 && item["above_recommended_price"] < 5) || (item["fade_percentage"].to_f >= 95 && item["above_recommended_price"] < 0.1)
-                 else
-                   (item["fade_percentage"].to_f >= 98 && item["above_recommended_price"] < 1) || (item["fade_percentage"].to_f >= 96 && item["above_recommended_price"] < -4)
-                 end
+      should_bid = if special
+                    if item["market_name"].include?("AWP")
+                      (item["fade_percentage"].to_f >= 99.8 && item["above_recommended_price"] < 20) || (item["fade_percentage"].to_f >= 98.3 && item["above_recommended_price"] < 10) || (item["fade_percentage"].to_f >= 96 && item["above_recommended_price"] < 2)
+                    else
+                      (item["fade_percentage"].to_f >= 99.8 && item["above_recommended_price"] < 30) || (item["fade_percentage"].to_f >= 99 && item["above_recommended_price"] < 20) || (item["fade_percentage"].to_f >= 98 && item["above_recommended_price"] < 10) || (item["fade_percentage"].to_f >= 95 && item["above_recommended_price"] < 1)
+                    end
+                  else
+                    if item["market_name"].include?("Paracord")
+                      (item["fade_percentage"].to_f >= 99 && item["above_recommended_price"] < 10) || (item["fade_percentage"].to_f >= 98 && item["above_recommended_price"] < 6) || (item["fade_percentage"].to_f >= 96 && item["above_recommended_price"] < 0.5)
+                    elsif item["market_name"].include?("Talon")
+                      (item["fade_percentage"].to_f >= 99 && item["above_recommended_price"] < 20) || (item["fade_percentage"].to_f >= 97.5 && item["above_recommended_price"] < 7) || (item["fade_percentage"].to_f >= 96 && item["above_recommended_price"] < 2)
+                    elsif item["market_name"].include?("Gut") || item["market_name"].include?("Navaja") || item["market_name"].include?("Flip")
+                      (item["fade_percentage"].to_f >= 99 && item["above_recommended_price"] < 7) || (item["fade_percentage"].to_f >= 98 && item["above_recommended_price"] < 4) || (item["fade_percentage"].to_f >= 96 && item["above_recommended_price"] < 0)
+                    else
+                      (item["fade_percentage"].to_f >= 99 && item["above_recommended_price"] < 10) || (item["fade_percentage"].to_f >= 98 && item["above_recommended_price"] < 6) || (item["fade_percentage"].to_f >= 96 && item["above_recommended_price"] < 0)
+                    end
+                  end
 
-    if should_bid
-      other_message = bid_for(item)
-      send_telegram_message(other_message) if other_message.present?
+      if should_bid
+        other_message = bid_for(item)
+        send_telegram_message(other_message) if other_message.present?
+      end
+    rescue StandardError => e
+      send_telegram_message("Error: #{e.message}")
     end
   end
 
@@ -30,7 +48,7 @@ class BidJobFade
     response = bid(item, item["purchase_price"])
 
     if response["success"]
-      return "Bid placed for #{item["market_name"]} with id #{item["id"]} amount #{item["purchase_price"].to_f / 160}."
+      return "Bid placed for #{item["market_name"]} with id #{item["id"]} amount #{item["purchase_price"].to_f / 162.8}."
     else
       return "Failed to bid on item #{item["market_name"]}. Error: #{response["message"]}"
     end
@@ -51,12 +69,15 @@ class BidJobFade
   end
 
   def item_message(item)
-    "ITEM FOUND #{item["market_name"]} with ID #{item["id"]} — Market: #{item["market_value"]}, Purchase: #{item["purchase_price"]}, Wear: #{item["wear"]}"
+    "#{ENV["next_buyer"].to_s == "0" ? "LUCAS" : "MATEO CHUPAVERGA"} FADE FOUND #{item["market_name"]} with ID #{item["id"]} — Market: #{item["market_value"]}, Purchase: #{item["purchase_price"]}, Wear: #{item["wear"]} Y FADE #{item["fade_percentage"]}"
   end
 
   def send_telegram_message(message)
-    Telegram::Bot::Client.run(TELEGRAM_TOKEN) do |bot|
+    Telegram::Bot::Client.run(TELEGRAM_TOKEN_2) do |bot|
       bot.api.send_message(chat_id: TELEGRAM_CHAT_ID, text: message)
+    end
+    Telegram::Bot::Client.run(TELEGRAM_TOKEN_2) do |bot|
+      bot.api.send_message(chat_id: TELEGRAM_CHAT_ID_BRO, text: message)
     end
   end
 end
---
+++ app/sidekiq/bid_job_keychain.rb
@@ -0,0 +1,72 @@
+require 'httparty'
+require 'telegram/bot'
+
+class BidJobKeychain
+  include Sidekiq::Job
+
+  API_URL = 'https://your-endpoint-x.com'
+  TELEGRAM_TOKEN = ENV["TELEGRAM_BOT_TOKEN"]
+  TELEGRAM_TOKEN_2 = ENV["TELEGRAM_BOT_TOKEN_2"]
+  TELEGRAM_CHAT_ID = ENV["TELEGRAM_CHAT_ID"]
+  TELEGRAM_CHAT_ID_BRO = ENV["TELEGRAM_CHAT_ID_BRO"]
+
+  def perform(item)
+    begin
+      message = item_message(item)
+      send_telegram_message(message) if message.present?
+
+      if (good_expensive(item) && item["purchase_price"] < 60000) || (item["above_recommended_price"] < 0 && item["purchase_price"] < 6000)
+        other_message = bid_for(item)
+        send_telegram_message(other_message) if other_message.present?
+      end
+    rescue StandardError => e
+      send_telegram_message("Error: #{e.message}")
+    end
+  end
+
+  private
+
+  def good_expensive(item)
+    names = ["Hot Howl", "Hot Wurst", "Baby Karat T", "Baby Karat CT", "Diamond Dog", "Semi-Precious"]
+
+    return false unless names.any? { |name| item["keychains"].present? && item["keychains"].first["name"]&.include?(name) }
+    (item["purchase_price"] - item["suggested_price"]).to_f / 162.8 < 10
+  end
+
+  def bid_for(item)
+    response = bid(item, item["purchase_price"])
+
+    if response["success"]
+      return "Bid placed for #{item["market_name"]} with id #{item["id"]} amount #{item["purchase_price"].to_f / 162.8}."
+    else
+      return "Failed to bid on item #{item["market_name"]}. Error: #{response["message"]}"
+    end
+  end
+
+  def bid(item, amount)
+    api_key = ENV["next_buyer"].to_s == "0" ? ENV["api_key"] : ENV["api_key_bro"]
+
+    HTTParty.post(
+      "https://csgoempire.com/api/v2/trading/deposit/#{item["id"]}/bid",
+      headers: {
+        "Authorization" => "Bearer #{api_key}",
+        "Content-Type" => "application/json",
+        "Accept" => "application/json"
+      },
+      body: { bid_value: amount }.to_json
+    )
+  end
+
+  def item_message(item)
+    "#{ENV["next_buyer"].to_s == "0" ? "LUCAS" : "MATEO CARA DE PENE"} KEYCHAIN SKIN FOUND #{item["market_name"]} with ID #{item["id"]} — Market: #{item["market_value"]}, Purchase: #{item["purchase_price"]}, Wear: #{item["wear"]}"
+  end
+
+  def send_telegram_message(message)
+    Telegram::Bot::Client.run(TELEGRAM_TOKEN_2) do |bot|
+      bot.api.send_message(chat_id: TELEGRAM_CHAT_ID, text: message)
+    end
+    Telegram::Bot::Client.run(TELEGRAM_TOKEN_2) do |bot|
+      bot.api.send_message(chat_id: TELEGRAM_CHAT_ID_BRO, text: message)
+    end
+  end
+end

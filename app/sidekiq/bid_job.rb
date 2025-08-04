4139da241e5f8d282531ade0cf296e5895f2490d
Lucas de sierra <83076120+ldesierra@users.noreply.github.com>
2025-08-04 12:05:08 -0300

    sdfsfdssdssdsdss

--- app/sidekiq/bid_job.rb
+++ app/sidekiq/bid_job.rb
@@ -6,7 +6,10 @@ class BidJob
 
   API_URL = 'https://your-endpoint-x.com'
   TELEGRAM_TOKEN = ENV["TELEGRAM_BOT_TOKEN"]
+  TELEGRAM_TOKEN_2 = ENV["TELEGRAM_BOT_TOKEN_2"]
   TELEGRAM_CHAT_ID = ENV["TELEGRAM_CHAT_ID"]
+  TELEGRAM_CHAT_ID_2 = ENV["TELEGRAM_CHAT_ID_2"]
+  TELEGRAM_CHAT_ID_BRO = ENV["TELEGRAM_CHAT_ID_BRO"]
 
   def perform(item, amount)
     if amount.present?
@@ -74,8 +77,8 @@ class BidJob
   end
 
   def send_telegram_message(message)
-    Telegram::Bot::Client.run(TELEGRAM_TOKEN) do |bot|
-      bot.api.send_message(chat_id: TELEGRAM_CHAT_ID, text: message)
+    Telegram::Bot::Client.run(TELEGRAM_TOKEN_2) do |bot|
+      bot.api.send_message(chat_id: TELEGRAM_CHAT_ID_2, text: message)
     end
   end
 end
--- app/sidekiq/secondly_job.rb
+++ app/sidekiq/secondly_job.rb
@@ -20,55 +20,53 @@ class SecondlyJob
   private
 
   def actual_call
-    messages = []
-    response = call_empire_api
+    begin
+      messages = []
+      response = call_empire_api
 
-    if response.code == 200
-      katowice_2015_items = get_katowice_2015_items(response)
-      katowice_2014_items = get_katowice_2014_items(response)
-      blue_gem_items = get_blue_gem_items(response)
-      nice_fade_items = get_nice_fade_items(response)
-      m4_and_awp_fade = get_m4_and_awp_fade(response)
-      low_floats = get_low_float_items(response)
-      high_float_items = get_high_float_items(response)
-      nice_gloves_items = get_nice_gloves_items(response)
+      if response.code == 200
+        katowice_2014_items = get_katowice_2014_items(response)
+        blue_gem_items = get_blue_gem_items(response)
+        nice_fade_items = get_nice_fade_items(response)
+        m4_and_awp_fade = get_m4_and_awp_fade(response)
+        low_floats = get_low_float_items(response)
+        nice_gloves_items = get_nice_gloves_items(response)
 
-      puts "TIME IS #{Time.now.hour}"
-      puts "MENOR A 5? #{Time.now.hour < 5}"
-      puts "MAYOR O IGUAL A 11? #{Time.now.hour >= 11}"
+        if blue_gem_items.any?
+          messages << "Blue gem items found: #{blue_gem_items.map { |item| "#{item["market_name"]} with id #{item["id"]} with blue percentage #{ item["blue_percentage"] }" }.join(', ')}"
+        elsif nice_fade_items.any? && (Time.now.hour < 5 || Time.now.hour >= 11)
+          nice_fade_items.each do |item|
+            BidJobFade.perform_async(item, false)
+          end
+        elsif nice_gloves_items.any? && (Time.now.hour < 5 || Time.now.hour >= 11)
+          nice_gloves_items.each do |item|
+            BidJobGloves.perform_async(item)
+          end
+        elsif m4_and_awp_fade.any? && (Time.now.hour < 5 || Time.now.hour >= 11)
+          m4_and_awp_fade.each do |item|
+            BidJobFade.perform_async(item, true)
+          end
+        elsif low_floats.present? && low_floats.any? && (Time.now.hour < 2 || Time.now.hour >= 8)
+          low_floats.each do |item|
+            BidJob.perform_async(item, nil)
+          end
+        elsif katowice_2014_items.any?
+          messages << "Katowice 2014 items found: #{katowice_2014_items.map { |item| "#{item["market_name"]} with id #{item["id"]} with stickers #{ item["stickers"]&.pluck("name") }" }.join(', ')}"
+        end
 
-      if blue_gem_items.any?
-        messages << "Blue gem items found: #{blue_gem_items.map { |item| "#{item["market_name"]} with id #{item["id"]} with blue percentage #{ item["blue_percentage"] }" }.join(', ')}"
-      elsif nice_fade_items.any? && (Time.now.hour < 5 || Time.now.hour >= 11)
-        nice_fade_items.each do |item|
-          BidJobFade.perform_async(item, false)
-        end
-      elsif nice_gloves_items.any? && (Time.now.hour < 5 || Time.now.hour >= 11)
-        nice_gloves_items.each do |item|
-          BidJobGloves.perform_async(item)
-        end
-      elsif m4_and_awp_fade.any? && (Time.now.hour < 5 || Time.now.hour >= 11)
-        m4_and_awp_fade.each do |item|
-          BidJobFade.perform_async(item, true)
-        end
-      elsif low_floats.present? && low_floats.any? && (Time.now.hour < 2 || Time.now.hour >= 8)
-        low_floats.each do |item|
-          BidJob.perform_async(item, nil)
-        end
-      elsif katowice_2014_items.any?
-        messages << "Katowice 2014 items found: #{katowice_2014_items.map { |item| "#{item["market_name"]} with id #{item["id"]} with stickers #{ item["stickers"]&.pluck("name") }" }.join(', ')}"
+        messages
+      else
+        puts "Error: #{response.code}"
       end
-
-      messages
-    else
-      puts "Error: #{response.code}"
+    rescue StandardError => e
+      send_telegram_message("Error: #{e.message}")
     end
   end
 
   def get_nice_gloves_items(response)
-    names = ["Specialist Gloves | Crimson Web", "Specialist Gloves | Marble Fade", "Hand Wraps | Slaughter", "Hand Wraps | Cobalt Skulls", "Driver Gloves | Imperial Plaid", "Driver Gloves | King Snake", "Sport Gloves | Nocts", "Specialist Gloves | Tiger Strike", "Driver Gloves | Snow Leopard"]
+    names = ["Specialist Gloves | Crimson Web", "Specialist Gloves | Marble Fade", "Sport Gloves | Omega", "Sport Gloves | Slingshot", "Sport Gloves | Amphibious", "Hand Wraps | Cobalt Skulls", "Driver Gloves | Imperial Plaid", "Driver Gloves | King Snake", "Sport Gloves | Nocts", "Specialist Gloves | Tiger Strike", "Sport Gloves | Vice", "Driver Gloves | Snow Leopard"]
     response["data"].filter { |item| names.any? { |name| item["market_name"].include?(name) } }
-                    .filter { |item| item["wear"].to_f >= 0.151 && item["wear"].to_f <= 0.18 }
+                    .filter { |item| item["wear"].to_f >= 0.151 && item["wear"].to_f <= 0.25 }
   end
 
   def get_katowice_2015_items(response)
